d88971a6a164f568a6fd7b7ba2718c2b6424e8e7
navodsand <sandaruwannavod223@gmail.com>
2023-07-31 20:44:52 +0530

    reactapp

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './NavBarStyle.css';
+
+
+export default function NavBar() {
+    return(
+        <div class="topnav">
+            <a class="active" href="">Home</a>
+            <a href="">Contact</a>
+            <a href="">About</a>
+            <a href="">Sign In</a>
+            <a href="">Login</a>
+        </div>
+    )
+}
