0d21bb196cc773ebcac33d3ec53b201b9f47c575
navodsand <sandaruwannavod223@gmail.com>
2023-08-10 18:43:04 +0530

    reactapp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from "./Components/NavBar/NavBar";
+import Container from "./Components/Container/Container";
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Container/>
     </div>
   );
 }
---
+++ src/Components/Container/Container.js
@@ -0,0 +1,19 @@
+import React from "react"
+import './ContainerStyle.css';
+
+
+export default function NavBar() {
+    return(
+        <div class="container">
+            <h1>Welcome to our Commercial Website!</h1>
+            {/* <ul class="item-list">
+                <li><img src="image1.jpg" ></li>
+                <li><img src="image2.jpg" alt="Item 2"></li>
+                <li><img src="image3.jpg" alt="Item 3"></li>
+                <li><img src="image4.jpg" alt="Item 4"></li>
+                <li><img src="image5.jpg" alt="Item 5"></li>
+                <li><img src="image6.jpg" alt="Item 6"></li>
+            </ul> */}
+        </div>
+    )
+}
