1dc813ae0a07cf25693d1350355f152f2d43ae59
navodsand <sandaruwannavod223@gmail.com>
2023-08-11 12:17:40 +0530

    reactapp

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@
 import './App.css';
 import NavBar from "./Components/NavBar/NavBar";
 import Container from "./Components/Container/Container";
+import { BrowserRouter as Roter,Route,Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar/>
-      <Container/>
-    </div>
+    <Roter>
+      <div>
+        <Switch>
+
+          <Route path='/'>
+            <Home/>
+          </Route>
+
+          <Route path='/About'>
+            <About/>
+          </Route>
+
+          <Route path='/Contact'>
+            <Contact/>
+          </Route>
+
+          <Route path='/Login'>
+            <Login/>
+          </Route>
+
+        </Switch> 
+      </div>
+    <Roter/>
   );
 }
 
