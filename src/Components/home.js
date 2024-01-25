d39c788ceec464cf7286da48c65a08bd66088bba
navodsand <sandaruwannavod223@gmail.com>
2024-01-25 15:05:13 +0530

    reactapp

---
+++ src/Components/home.js
@@ -0,0 +1,110 @@
+//import logo from './logo.svg';
+import './home.css';
+
+function home() {
+  return (
+    <div className="App">
+      <body>
+    <div class="backimage">
+      <header>
+        <div class="topnav">
+          <a class="active" href="index.html">Home</a>
+          <a href="about.html">About</a>
+          <a href="Login.html">Login</a>
+        </div>
+      </header>
+
+      <main class="home">
+        {/* <!-- Home page content --> */}
+        <section class="welcome-section">
+          <div class="title"><h1>Welcome to Our Restaurant</h1></div>
+        </section>
+        <section class="items">
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+          <div class="item">
+            <img src="item1.jpg" />
+            <h4>Mix Rice</h4>
+            <p>$20.00</p>
+            <button>Buy</button>
+          </div>
+        </section>
+      </main>
+
+      <footer>
+        <p>Contact us at: contact@restaurant.com</p>
+        {/* <!-- Add more footer content --> */}
+      </footer>
+
+      <script src="script.js"></script>
+    </div>
+  </body>
+    </div>
+  );
+}
+
+export default home;
